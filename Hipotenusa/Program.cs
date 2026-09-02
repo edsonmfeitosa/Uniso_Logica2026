496a505965441fdf3e4ce25aab98d18902dc68ec
edson <edson.feitosa@hotmail.com>
2026-09-02 20:32:28 -0300

    primeira aula de programação de pseudo para código

---
+++ Hipotenusa/Program.cs
@@ -0,0 +1,21 @@
+﻿double cat1;
+double cat2;
+double hipo;
+
+//Escreva("Digite o 1º cateto")
+Console.WriteLine("Digite o 1º cateto");
+//Leia(cat1)
+cat1 = Convert.ToDouble(Console.ReadLine());
+
+//Escreva("Digite o 2º cateto")
+Console.WriteLine("Digite o 2º cateto");
+//Leia(cat2)
+cat2 = Convert.ToDouble(Console.ReadLine());
+
+//hipo = raiz(cat1² + cat2²)
+// ^ significa "elevado a" no C# Math.Pow
+//Math.Sqrt = raiz quadrada
+hipo = Math.Sqrt(Math.Pow(cat1, 2) + Math.Pow(cat2, 2));
+
+//Escreva("A hipotenusa é: " + hipo)
+Console.WriteLine("A hipotenusa é: " + hipo);
---
+++ SucessorAntecessor/SucessorAntecessor/Program.cs
@@ -0,0 +1,8 @@
+﻿int numero;
+
+Console.WriteLine("Digite um número:");
+numero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("O sucessor do número " + numero + " é: " + 
+    (numero + 1));
+Console.WriteLine("O antecessor do número " + numero + " é: " + 
+    (numero - 1));
